fe7530137f6abada780fcd55fa1b7c9740c99198
abdeldjallil <abdeldjallil_ferchouche@hotmail.com>
2016-11-13 01:48:21 +0100

    first commit

---
+++ Domain/src/main/java/com/founisseur/model/Product.java
@@ -0,0 +1,47 @@
+package com.founisseur.model;
+
+/**
+ * Created by Abdeldjallil on 13/11/2016.
+ */
+public class Product {
+
+    private long id;
+    private String name;
+    private String description;
+
+
+    public Product() {
+    }
+
+    public Product(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/main/java/com/alma/fournisseur/App.java
@@ -0,0 +1,13 @@
+package com.alma.fournisseur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
